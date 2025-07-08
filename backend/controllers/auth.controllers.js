9b28e3492baad13a2b8a535f6d30de79534f3d5a
Tania Devi <taniarajput303@gmail.com>
2025-07-08 15:20:30 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -47,11 +47,11 @@ export const signup = async (req, res) => {
 
         // Set cookie
         res.cookie("token", token, {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "Lax",
-            secure: false
-        });
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  sameSite: "None", // ✅
+  secure: true      // ✅
+});
 
         return res.status(201).json(user);
 
@@ -81,12 +81,13 @@ export const Login = async (req, res) => {
         const token = await genToken(user._id);
 
         // Set cookie
-        res.cookie("token", token, {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "Lax",
-            secure: false
-        });
+       res.cookie("token", token, {
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  sameSite: "None", // ✅
+  secure: true      // ✅
+});
+
 
         return res.status(200).json(user);
 
